4d9698fde668617d5357ad8cf3e4cbe5781e8f8a
alenabasanets2019 <alonabasanec@gmail.com>
2020-03-02 21:25:45 +0200

    add unit test

---
+++ calc/calc/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calc
+{
+    public class Calc
+    {
+        public int x;
+        public int y;
+        public int Sum (int x,int y)
+        {
+            return x + y;
+        }
+        public int Mul(int x, int y)
+        {
+            return x*y;
+        }
+        public int square(int x)
+        {
+            return x*x;
+        }
+        public double Root(int x)
+        {
+            return Math.Pow(x, 0.5);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calc c = new Calc();
+            Console.WriteLine(c.Root(225));
+        }
+    }
+}
